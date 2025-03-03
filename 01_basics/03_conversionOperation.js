d22f86beeb4c42a18f7be8e532295c8371998be8
hardikk258 <hardiksingh241@gmail.com>
2025-03-03 12:46:33 +0530

    conversion done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,2 +1,31 @@
-console.log("hardik");
-console.log("demo checking");
+// console.log("hardik");
+// console.log("demo checking");
+
+// let score = "33"
+let score = "33abc"
+console.log(score);
+console.log(typeof(score));
+
+let valueinnum = Number(score);
+console.log(typeof(valueinnum));
+console.log(valueinnum);  // nan bcoc 33abc is not a pure number
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+
+
+let islogged = "1"
+let booleanislogged = Boolean(islogged);
+console.log(typeof(booleanislogged));
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+console.log(typeof (stringNumber));

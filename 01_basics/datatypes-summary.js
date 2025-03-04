133032d5bf14c25a8385c4a343ee731c87278d03
hardikk258 <hardiksingh241@gmail.com>
2025-03-04 13:10:35 +0530

    datatype summary done

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,36 @@
+// primitive  // 2 categories based on kaisae data memory m store n access honge
+
+// 7 String, Number, Boolean, null, undefined, Symbol, BigInt  // call by value
+
+const score = 100  //number
+const scoreValue = 100.3 //number
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+
+
+
+// Reference (Non primitive)
+
+const heroes = ["shaktiman,", "naagraj", "dogs"];
+let obj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
+
+// Array, Objects, Functions

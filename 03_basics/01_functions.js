ac720abe08dab7b8a8608b58e482f6644d4a35cf
hardikk258 <hardiksingh241@gmail.com>
2025-07-15 16:05:50 +0530

    func with object done

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -38,3 +38,32 @@ function loginUserMessage(username = "sam"){
 console.log(loginUserMessage("hardik"));
 
 
+
+function calculateCartPrice(val1, val2, ...num1){   // rest operator // spread operator
+    return num1
+}
+console.log(calculateCartPrice(2,4,5,6));
+
+
+const user = {
+    username : "hardik",
+    price : 199
+}
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+    
+}
+handleObject(user);
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+
+const myNewArray = [200, 400, 100, 600]
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200,400,100,600]));
+
